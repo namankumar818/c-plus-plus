36ba6c7de87367dff4ccb9b4c487cf93f74bff18
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2021-06-11 21:58:37 +0530

    temperature converter (c in f)

---
+++ temperature convert.cpp
@@ -0,0 +1,20 @@
+// temperature converter
+using namespace std;
+#include<iostream>
+int main()
+{
+ double T; 
+ double c;
+ cout<<"enter temp in f:- ";
+ cin>>T;
+
+c = (T-32)*5/9;
+cout<<c;
+
+
+
+
+
+
+return 0;
+}
