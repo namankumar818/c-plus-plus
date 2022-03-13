eecd9eda19c3b0b339aada7082312c6bbca2c520
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2022-03-14 00:14:35 +0530

    evaluation of prefix notation

---
+++ prefix evaluation using stck.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<stack>
+#include<math.h>
+using namespace std;
+int prefixeval(string s){
+stack<int> st;
+for(int i = s.size()-1;i>=0;i--){
+if(s[i] >='0' && s[i] <= '9'){
+st.push(s[i] - '0');
+}
+else{
+int op2 = st.top();
+st.pop();
+int op1 = st.top();
+st.pop();
+switch(s[i]){
+case '+':
+st.push(op1 + op2);
+break;
+case '-':
+st.push(op1 - op2);
+break;
+case '*':
+st.push(op1 * op2);
+break;
+case '/':
+st.push(op1 / op2);
+break;
+case '^':
+st.push(pow(op1, op2));
+break;
+}
+}
+}
+return st.top();
+}
+
+int main(){
+string s;
+cin>>s;
+cout<<prefixeval(s)<<endl;
+
+return 0;
+}
